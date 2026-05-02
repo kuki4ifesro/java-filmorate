627fd1a65dbadd83479f40fa78099bf8b4e8b3a0
kuka <zapbarbekk@gmail.com>
2026-05-02 13:25:04 +0500

    надеюсь без ошибок

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO mpa (id, name) VALUES 
+MERGE INTO mpa (id, name) KEY (id) VALUES
 (1, 'G'),
 (2, 'PG'),
 (3, 'PG-13'),
 (4, 'R'),
 (5, 'NC-17');
 
-INSERT INTO genres (id, name) VALUES 
+MERGE INTO genres (id, name) KEY (id) VALUES
 (1, 'Комедия'),
 (2, 'Драма'),
 (3, 'Триллер'),
