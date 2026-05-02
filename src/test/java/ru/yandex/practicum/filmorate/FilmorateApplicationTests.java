f5ccc9e29ee21ad4c9f44b215494359748905202
kuka <zapbarbekk@gmail.com>
2026-05-02 12:50:11 +0500

    попытка 10

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -2,13 +2,10 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import ru.yandex.practicum.filmorate.config.TestStorageConfig;
 
 @SpringBootTest(properties = {
 	"spring.datasource.url=jdbc:h2:mem:testdb"
 })
-@Import(TestStorageConfig.class)
 class FilmorateApplicationTests {
 
 	@Test
--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -25,7 +24,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 	"spring.datasource.url=jdbc:h2:mem:testdb"
 })
 @AutoConfigureMockMvc
-@Import(ru.yandex.practicum.filmorate.config.TestStorageConfig.class)
 class FilmControllerTest {
 
 	@Autowired
